21a3259cb863d1a74d410b73e84bd744f33ea627
Rishi Pandey <Rishi Pandey@DESKTOP-0N4HF0R>
2023-10-01 15:38:02 +0530

    Add project files.

---
+++ ePizzaHub.Repositories/Implementations/Respository.cs
@@ -0,0 +1,43 @@
+﻿using ePizzaHub.Repositories.Interfaces;
+
+
+namespace ePizzaHub.Repositories.Implementations
+{
+    public class Respository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        public void Add(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> Find(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ePizzaHub.Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿
+
+namespace ePizzaHub.Repositories.Interfaces
+{
+    public interface IRepository<TEntity> where TEntity : class
+    {
+        IEnumerable<TEntity> GetAll();
+        IEnumerable<TEntity> Find(object id);
+        void Add(TEntity entity);
+        void Update(TEntity entity);
+        void Remove(TEntity entity);
+        void Delete(object id);
+        int SaveChanges();
+
+    }
+}
